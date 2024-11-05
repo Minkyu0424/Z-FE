2596162cd18ca94ef1053bf780d06be42fc7035a
Minkyu0424 <yng1404@gmail.com>
2024-11-05 23:37:26 +0900

    19 feat : 검색결과 없을 시 UI

--- src/app/constants/search.ts
+++ src/app/constants/search.ts
@@ -1,3 +1,5 @@
 export const SEARCH_PLACEHOLDER = '원하는 검색어를 입력해주세요';
 
 export const SEARCH_TYPES: SearchTypes[] = ['POST', 'PEOPLE'];
+
+export const NO_RESULT_TEXT = '검색어와 일치하는 게시물이 존재하지 않습니다. 다른 검색어로 시도해보세요!';
