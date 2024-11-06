975f6cc5fb96d37bcb7bde0296d1c42de4fa92e8
Minkyu0424 <yng1404@gmail.com>
2024-11-06 14:04:06 +0900

    21 feat : 게시글 삭제 모달

---
+++ src/app/components/edit/PostDeleteModal.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { DELETE_MODAL_TEXT } from '@/app/constants/post';
+import Button from '../common/ui/Button';
+interface PostDeleteModalProps {
+  postId: string;
+  closeModal: () => void;
+}
+
+const PostDeleteModal = ({ postId, closeModal }: PostDeleteModalProps) => {
+  return (
+    <div className="fixed inset-0 flex-center bg-main-3 bg-opacity-10 z-50">
+      <div className="w-[340px] h-[284px] gap-y-4 px-7 py-[30px] bg-black rounded-xl shadow flex flex-col text-white">
+        <p className="text-2xl font-semibold">{DELETE_MODAL_TEXT[0]}</p>
+        <p className="flex flex-wrap text-[15px] font-light">{DELETE_MODAL_TEXT[1]}</p>
+        <div className="self-stretch h-[76px] flex-col justify-start items-start gap-3 flex mt-5">
+          <Button
+            buttonText="삭제"
+            type="deleteModal"
+            className="bg-[#E62222] text-white"
+            onClickHandler={closeModal}
+          />
+          <Button buttonText="취소" type="deleteModal" className="bg-white text-main-0" onClickHandler={closeModal} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostDeleteModal;
--- src/app/constants/post.ts
+++ src/app/constants/post.ts
@@ -3,3 +3,8 @@ export const POST_COMMENT_PLACEHOLDER = '댓글을 남겨보세요';
 export const REPOST_TITLE = 'NewPost';
 
 export const REPOST_PLACEHOLDER = '당신만의 게시물을 작성해보세요';
+
+export const DELETE_MODAL_TEXT = [
+  '게시물을 삭제하시겠습니까?',
+  '이 작업은 되돌릴 수 없으며, 회원님의 프로필과 팔로워들의 타임라인, 그리고 검색 결과에서 삭제됩니다.',
+];
