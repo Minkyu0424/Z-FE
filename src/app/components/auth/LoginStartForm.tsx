394ae00b9ab6655a58601507d99d531e13095dd5
Minkyu0424 <yng1404@gmail.com>
2024-11-07 01:42:11 +0900

    10 refactor : develop 머지

--- src/app/components/auth/LoginStartForm.tsx
+++ src/app/components/auth/LoginStartForm.tsx
@@ -1,36 +1,28 @@
 'use client';
 
-import React, { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 const client_id = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID || '';
 const redirect_uri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI || '';
-const response_type = process.env.KAKAO_RESPONSE_TYPE || 'code';
 
 const LoginStartForm: React.FC = () => {
-  const handleKakaoLogin = () => {
-    const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=${response_type}`;
-    window.location.href = kakaoAuthUrl;
-  };
+  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=code`;
 
   return (
-    <div className="flex flex-col justify-center items-center  min-h-screen">
-      <div className="flex flex-col justify-center items-center bg-gray-100 gap-10">
-        <div className="flex justify-center items-center">
-          <Image src="/logo_Z.jpg" alt="Logo" width={200} height={200} className="mx-auto" />
-        </div>
-        <div className="flex flex-col text-centers justify-center items-center gap-7">
-          <h2>Z에서 무슨일이???</h2>
-          <h5>지금 바로 시작해보세요!</h5>
-        </div>
-        <div className="flex justify-cener items-center">
-          <button onClick={handleKakaoLogin} className="py-200 px- rounded-md w-64 flex items-center justify-center">
-            <Image src="/kakao_login_large_narrow.png" alt="Kakao Logo" width={160} height={200} />
-          </button>
-        </div>
+    <div className="flex w-[400px] flex-col items-center justify-center min-h-screen gap-y-[60px]">
+      <Image src="/logo_Z.jpg" alt="Logo" width={240} height={240} />
+      <div className="flex flex-col text-4xl items-center font-semibold gap-y-10">
+        <h2>Z에서 무슨일이?</h2>
+        <h5 className="text-3xl">지금 바로 시작해보세요!</h5>
       </div>
+      <Link href={kakaoAuthUrl}>
+        <Image src="/kakao_login_large_narrow.png" alt="Kakao Logo" width={224} height={55} />
+      </Link>
     </div>
   );
 };
 
 export default LoginStartForm;
+
